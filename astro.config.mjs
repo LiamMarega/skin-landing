50a52fef551734d3fa022fa83f91820ee218d698
LiamMarega <liammarega85@gmail.com>
2026-02-07 03:18:01 -0300

    output: 'static',

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: node({
     mode: 'standalone'
   }),
